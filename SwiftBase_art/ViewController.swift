a7a651e2e0a2c9b8ccd4c6f5c86eab8877421b22
HoangNguyenTomosia <hoang.nguyen1@tomosia.com>
2020-11-13 14:54:28 +0700

    Initial Commit

---
+++ SwiftBase_art/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBase_art
+//
+//  Created by MacBook on 11/13/20.
+//  Copyright © 2020 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
