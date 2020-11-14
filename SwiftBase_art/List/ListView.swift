0241090ae9e7132bff1ceeb94172273935cb8011
HoangNguyenTomosia <hoang.nguyen1@tomosia.com>
2020-11-14 12:16:05 +0700

    add backbtn in list scr

--- SwiftBase_art/List/ListView.swift
+++ SwiftBase_art/List/ListView.swift
@@ -17,11 +17,28 @@ class ListView: BaseView {
         return table
     }()
     
+    lazy var backBtn: UIButton = {
+       let btn = UIButton()
+        btn.setTitle("BACK", for: .normal)
+        btn.backgroundColor = .orange
+        return btn
+    }()
+    
     override func setupView() {
+        self.backgroundColor = .white
+        
+        self.addSubview(backBtn)
+        backBtn.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self)
+            make.width.equalTo(50)
+            make.height.equalTo(30)
+            make.top.equalTo(self).offset(100)
+        }
         self.addSubview(tableView)
         tableView.snp.makeConstraints { (make) in
+            make.top.equalTo(backBtn.snp.bottom).offset(20)
             make.width.equalToSuperview()
-            make.height.equalToSuperview()
+            make.bottom.equalTo(self.snp.bottom)
         }
     }
 }
